b3da1fbf098b0c47418c9bb2a5d497180cad17ff
exnaton18 <exnaton18@gmail.com>
2016-06-23 15:33:43 +0300

    add header

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>News about all</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <span>N</span>ews
+        </div>
+        <div id="menuHead">
+            About us | Feedback
+        </div>
+        <div id="regAuth">
+            Registration | Authorisation
+        </div>
+    </header>
+</body>
+</html>
